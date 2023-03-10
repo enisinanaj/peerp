40e688f29cee13b1a8b5535c7ac7739f061962ec
Eni Sinanaj <eni.sinanaj@erstegroup.com>
2023-03-10 11:55:27 +0100

    initial commit
    Signed-off-by: Eni Sinanaj <eni.sinanaj@erstegroup.com>

---
+++ src/main/java/com/example/peerp/PeerpApplication.java
@@ -0,0 +1,13 @@
+package com.example.peerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/peerp/PeerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
